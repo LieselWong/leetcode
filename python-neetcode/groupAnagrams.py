5e7703361256ff29074eb20125e66684454c9779
LieselWong <liesel.wong20@gmail.com>
2023-06-03 22:16:23 -0400

    new problem

--- python-neetcode/groupAnagrams.py
+++ python-neetcode/groupAnagrams.py
@@ -7,32 +7,30 @@
  # Input: strs = ["eat","tea","tan","ate","nat","bat"]
  # Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 
-def isAnagram(s, t):
-    if (sorted(s) == sorted(t)): 
-        return True
-    return False
-
-def groupAnagrams(strs): 
+# O(nlogn)
+# Ω(n)
+class Solution(object):
+    def groupAnagrams(self, strs):
         """
-        type strs: List[str]
+        :type strs: List[str]
+        :rtype: List[List[str]]
         """
-        final = []
-        part = []
-        checked = []
-        for str in strs: 
-            if str not in checked: 
-                part = []
-                part.append(strs)
-                checked.append(strs)
-                for i in range(len(strs)): 
-                    if (isAnagram(str, strs[i]) and strs[i] != str): 
-                        checked.append(strs[i])
-                        part.append(strs[i])
-                        print("LFNFAOAOO")
-                        print(part)
-            final.append(part)
-        return final
+        # create a hash table {sorted value of anagrams: list of anagrams}
+        d = {} 
+        # for each strs[i]
+        for s in strs: 
+            # find sorted value, returns an array of each character
+            # joins all items in a tuple / array into a string
+            doink = "".join(sorted(s))
 
-
-strs = ["eat","tea","tan","ate","nat","bat"]
-print(groupAnagrams(strs))
+            d[doink] = d.get(doink, []) + [s]
+            # check if sorted string is in the hash table using get 
+            # if (d.get(doink) != None):     
+            # # if TRUE: append to list 
+            #     d[doink].append(s)
+            # else: 
+            #     # if FALSE: add a new value into hash table 
+            #     #           {sorted value of strs[i]: [strs[i]]}
+            #     d[doink] = [s]
+        # return list of values
+        return d.values()
